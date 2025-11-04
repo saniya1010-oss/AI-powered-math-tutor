afc79430a5109136fe02cf4f8b7e3a889574ecb9
Saniya Sayyed <sayyedsaniya1010@gmail.com>
2025-11-04 22:07:00 +0530

    Refactor synthetic_training_dataset.py for clarity
    Updated the difficulty level hierarchy comment and made the number of players adjustable.

--- src/synthetic_training_dataset.py
+++ src/synthetic_training_dataset.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 
 # number of players
-n = 1000
+n = 1000 #adjustable
 accuracy = np.random.normal(70, 15, n).clip(0, 100)
 avg_time = np.random.normal(8, 3, n).clip(1, 20)
 
-# Define "difficulty level" heuristically
+# ---Hierarchy of Difficulty Levels---
 difficulty = np.where(
     (accuracy > 90) & (avg_time < 5), 3, #extreme
     np.where(
@@ -14,8 +14,8 @@ difficulty = np.where(
             np.where((accuracy > 60) & (avg_time < 20), 1,  # medium
                     0 #easy
             )
+        )
     )
-  )
 
 df = pd.DataFrame({
     'accuracy': accuracy,
