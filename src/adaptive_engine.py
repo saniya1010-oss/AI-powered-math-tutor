0dcc070dfe37e9c811f1ca8084dfcc8206ad58e1
Saniya Sayyed <sayyedsaniya1010@gmail.com>
2026-01-28 13:27:11 +0530

    Replace LogisticRegression with DecisionTreeClassifier
    Refactor AdaptiveEngine to use DecisionTreeClassifier and update feature names.

--- src/adaptive_engine.py
+++ src/adaptive_engine.py
@@ -1,17 +1,16 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DesicionTreeClassifier
 from synthetic_training_dataset import get_dataset
-import numpy as np
-import pandas as pd
 
 class AdaptiveEngine:
     def __init__(self):
-        self.model = LogisticRegression()
+        self.model = DecisionTreeClassifier()
         df = get_dataset()
-        X = df[['accuracy', 'avg_time']]
+        X = df[['Accuracy', 'Time taken']]
         y = df['difficulty']
-        self.model.fit(X, y)
+        self.model.fit(X,y)
 
-    def predict_next_level(self, accuracy, avg_time):
-        pred = self.model.predict([[accuracy, avg_time]])[0]
-        return ['easy', 'medium', 'hard', 'extreme'][pred]
+def predict_difficulty(self, accuracy, time_taken):
+    features = [[accuracy, time_taken]]
+    prediction = self.model.predict(features)
+    return prediction[0]
 
